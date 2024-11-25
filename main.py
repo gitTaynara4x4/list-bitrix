3350a774c0f137c1cc3eb38813daf7f5becf1a8b
gitTaynara4x <git.taynara.4x4@gmail.com>
2024-11-25 12:07:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ def index():
     return jsonify({"message": "API para atualizar campos do Bitrix24."})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8858)
